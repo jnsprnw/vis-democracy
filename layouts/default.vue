1a00f9fd53521c4c856e38efb5d69423cb317431
Jonas <jonasparnow@gmail.com>
2018-02-14 23:04:23 +0100

    without header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="page">
-    <header class="page-header">
-      <nuxt-link to="/"><h1>Country Democracy Compare</h1></nuxt-link>
-    </header>
     <nuxt/></nuxt>
   </div>
 </template>
