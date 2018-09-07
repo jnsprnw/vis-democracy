4d6e9ee694e747e2464d0350d6c7f39ad834cd64
Jonas <jonasparnow@gmail.com>
2018-09-07 21:24:32 +0200

    hover link styling

--- components/LinkHover.vue
+++ components/LinkHover.vue
@@ -1,5 +1,5 @@
 <template>
-  <span v-on:mouseover="makeActiveCountry([path, value])" v-on:mouseleave="makeActiveCountry(false)" class="link-hover">{{ label }}</span>
+  <span v-on:mouseover="makeActiveCountry([path, value])" v-on:mouseleave="makeActiveCountry(false)" :class="classNames">{{ label }}</span>
 </template>
 
 <script>
@@ -7,6 +7,23 @@
 
   export default {
     props: ['path', 'value', 'label'],
+    computed: {
+      classNames () {
+        const { value } = this
+        let cls = 'link-hover'
+        if (value === 'Full democracy') {
+          cls += ' full-democracy'
+        } else if (value === 'Flawed democracy') {
+          cls += ' flawed-democracy'
+        } else if (value === 'Hybrid regime') {
+          cls += ' hybrid-regime'
+        } else if (value === 'Authoritarian') {
+          cls += ' authoritarian'
+        }
+        console.log(this.value)
+        return cls
+      }
+    },
     methods: {
       ...mapActions([
         'makeActiveCountry'
@@ -14,3 +31,45 @@
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  .link-hover {
+    border-bottom: 1px dashed #EC3A4D;
+
+    &:hover, &:focus {
+      border-bottom-style: solid;
+      color: #EC3A4D;
+    }
+
+    &.full-democracy {
+      font-weight: bold;
+      color: darken(#c2d22f, 10%);
+      // border-bottom: 2px solid #c2d22f;
+      border-color: #c2d22f;
+    }
+    &.flawed-democracy {
+      // font-weight: bold;
+      // color: #CCC200;
+      font-weight: bold;
+      color: #329967;
+      border-color: #7dbb45;
+      // border-bottom: 2px solid #7dbb45;
+    }
+    &.hybrid-regime {
+      font-weight: bold;
+      color: #117575;
+      border-color: #117575;
+      // font-weight: bold;
+      // color: #CCC200;
+      // border-bottom: 2px solid #117575;
+    }
+    &.authoritarian {
+      font-weight: bold;
+      color: #a51916;
+      border-color: #a51916;
+      // font-weight: bold;
+      // color: #CCC200;
+      // border-bottom: 2px solid #a51916;
+    }
+  }
+</style>
