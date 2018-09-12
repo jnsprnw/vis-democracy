ca5ea1d4fddcca106c55197806cb621ed7f5ce07
Jonas <jonasparnow@gmail.com>
2018-09-12 10:26:07 +0200

    display large image

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
       description: 'How democratic is the world? How many people life in a regime? And how strong is the economy of authoritian states? This interactive visualisation gives a new perspective on countries.',
       url: 'https://democracy.visualising.systems', // without trailing slash!
       color: '#EC3A4D',
-      card: 'summary',
+      card: 'summary_large_image',
       type: 'website',
       imprint: '/imprint',
       image: {
